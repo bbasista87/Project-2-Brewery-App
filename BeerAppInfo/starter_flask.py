76a5c339831086226076fc16a9c6f528673f42b8
Mac User <macuser@MacBook-Pro.local>
2018-12-15 13:20:45 -0500

    BeeryStuff

---
+++ BeerAppInfo/starter_flask.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+from flask import (
+    Flask,
+    render_template,
+    jsonify)
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# The database URI
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///beer.sqlite"
+
+db = SQLAlchemy(app)
+
+# Create database tables
+@app.before_first_request
+def setup():
+    # Recreate database each time for demo
+    #db.drop_all()
+    db.create_all()
+
+
+@app.route("/")
+def home():
+
+    # Find data
+    review_data = SQLAlchemy.db.wine_review.find_one()
+    print(review_data)
+    # return template and data
+    #return render_template("index.html", beer_info = beer_info)
+
+if __name__ == '__main__':
+    app.run(debug=True)
